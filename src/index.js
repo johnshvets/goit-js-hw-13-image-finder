336ab5a04e7dd89b7a82818a403674ef584a4173
Ivan Shvets <johnshvets@gmail.com>
2020-11-15 12:35:26 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './styles.css';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+import 'material-design-icons/iconfont/material-icons.css';
+import 'basiclightbox/dist/basicLightbox.min.css';
+import './js/searchImages';
+import './js/modal';
