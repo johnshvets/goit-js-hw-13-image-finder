97d98d33380ed14381d2f7f0e69b587d79f276e2
Ivan Shvets <johnshvets@gmail.com>
2020-11-15 12:35:36 +0200

    Create refs.js

---
+++ src/js/refs.js
@@ -0,0 +1,5 @@
+export default {
+  imagesGallery: document.querySelector('.gallery'),
+  searchImagesForm: document.querySelector('#search-form'),
+  gallery: document.querySelector('.gallery'),
+};
